36f552673b1e05cf055ffc973e7084ecd5f26c03
Shane Banks <shane.banks02@gmail.com>
2022-07-07 15:00:46 -0500

    Where exercise complete

--- where_exercises.sql
+++ where_exercises.sql
@@ -9,11 +9,10 @@ SELECT * FROM employees WHERE last_name LIKE 'E%';
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 
 SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
+
 SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE 'E%e';
 
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
-
-
-
 
 
