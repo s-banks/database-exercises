a35d5bc6d0f899b4fe08b965c06719091e24bbd6
s-banks <shroud2@hotmail.com>
2022-07-11 21:59:04 -0500

    exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -21,6 +21,23 @@ AND e.gender = 'F'
 ORDER BY d.dept_name;
 
 
-SELECT t.title AS title, COUNT(de.dept_emp) AS Total
-FROM employees as e
-        JOIN titles as t
+SELECT titles.title AS 'Title', COUNT(de.emp_no) AS 'TOTAL'
+FROM titles
+         JOIN dept_emp as de
+              ON titles.emp_no = de.emp_no
+         JOIN departments as d
+              ON de.dept_no = d.dept_no
+WHERE titles.to_date = '9999-01-01'
+  AND de.to_date = '9999-01-01'
+  AND d.dept_name = 'Customer Service'
+GROUP BY titles.title;
+
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary AS 'Salary'
+FROM departments as d
+         JOIN dept_manager as dm
+              ON d.dept_no = dm.dept_no
+         JOIN employees as e
+              ON dm.emp_no = e.emp_no
+         JOIN salaries as s
+              ON e.emp_no = s.emp_no
+WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01';
--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,2 +1,26 @@
 USE employees;
 
+SELECT first_name
+FROM employees
+WHERE hire_date IN (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = 101010
+);
+
+SELECT title
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+);
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE gender= 'f' AND to_date
+        = '9999-01-01'
+);
